68f4a25ab0eab752cf2daeea4dfc5aaa4eaa5ff4
오동욱 <dhfmepd@gmail.com>
2021-03-11 23:39:30 +0900

    3/11 댓글 기능 추가

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,5 @@ app_name = 'board'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:board_id>/', views.detail, name='detail'),
+    path('reply/create/<int:board_id>/', views.reply_create, name='reply_create'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Board
 
 def index(request):
@@ -16,3 +17,11 @@ def detail(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
     context = {'board': board}
     return render(request, 'board/board_detail.html', context)
+
+def reply_create(request, board_id):
+    """
+    Board 답변등록
+    """
+    board = get_object_or_404(Board, pk=board_id)
+    board.reply_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('board:detail', board_id=board.id)
