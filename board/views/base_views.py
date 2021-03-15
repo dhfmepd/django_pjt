e2dd8c77f8e8b1654a4ee2db213de7a65c85edef
오동욱 <dhfmepd@gmail.com>
2021-03-15 17:23:16 +0900

    3/15 상세 페이징 처리 및 정렬 추가

--- board/views/base_views.py
+++ board/views/base_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, Count
 from common.models import Menu
 from ..models import Board
 
@@ -13,9 +13,16 @@ def list(request, menu_id):
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
 
-    # 조회
-    board_list = Board.objects.filter(menu=menu_id).order_by('-create_date')
+    # 정렬
+    if so == 'recommend':
+        board_list = Board.objects.filter(menu=menu_id).annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        board_list = Board.objects.filter(menu=menu_id).annotate(num_reply=Count('reply')).order_by('-num_reply', '-create_date')
+    else:  # recent
+        board_list = Board.objects.filter(menu=menu_id).order_by('-create_date')
+
     if kw:
         board_list = board_list.filter(
             Q(subject__icontains=kw) |  # 제목검색
@@ -30,7 +37,7 @@ def list(request, menu_id):
 
     menu = Menu.objects.get(id=menu_id)
 
-    context = {'board_list': page_obj, 'page': page, 'kw': kw, 'menu': menu}  # page와 kw가 추가되었다.
+    context = {'board_list': page_obj, 'page': page, 'kw': kw, 'so': so, 'menu': menu}
     return render(request, 'board/board_list.html', context)
 
 @login_required(login_url='common:login')
@@ -38,6 +45,21 @@ def detail(request, board_id):
     """
     Board 상세 출력
     """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    # 조회
     board = get_object_or_404(Board, pk=board_id)
-    context = {'board': board}
+
+    # 정렬
+    if so == 'recommend':
+        reply_list = board.reply_set.all().annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    else:  # recent
+        reply_list = board.reply_set.all().order_by('-create_date')
+
+    paginator = Paginator(reply_list, 5)  # 페이지당 5개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'board': board, 'reply_list': page_obj, 'so': so}
     return render(request, 'board/board_detail.html', context)
