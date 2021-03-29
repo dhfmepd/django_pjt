210535310ab89bb4f9fb723e84d39b4817f9acdd
오동욱 <dhfmepd@gmail.com>
2021-03-29 17:20:27 +0900

    3/29 차트 샘플 추가 개선.

--- sample/views.py
+++ sample/views.py
@@ -7,6 +7,8 @@ from django.db.models.functions import Coalesce
 def chart_js(request):
     pie_label = []
     pie_data = []
+    user_a_label = ''
+    user_b_label = ''
     user_a_data = []
     user_b_data = []
 
@@ -23,26 +25,27 @@ def chart_js(request):
         pie_data.append(pie_dset['data'])
 
     bar_qset = Board.objects.values('author').annotate(
+        label=Subquery(user_sub_qset.values('username')[:1]),
         b_cnt=Count('id'),
         c_cnt=Coalesce(Subquery(Comment.objects.filter(author=OuterRef("author")).values('author').annotate(count=Count('id')).values('count')), 0),
         r_cnt=Coalesce(Subquery(Reply.objects.filter(author=OuterRef("author")).values('author').annotate(count=Count('id')).values('count')), 0),
     )
-    print(bar_qset)
+
     for bar_dset in bar_qset:
-        if bar_dset['author'] == 1:
+        # 테스트 용으로 2,3위 선택
+        if bar_dset['author'] == 4:
+            user_a_label = bar_dset['label']
             user_a_data.append(bar_dset['b_cnt'])
             user_a_data.append(bar_dset['c_cnt'])
             user_a_data.append(bar_dset['r_cnt'])
 
-        if bar_dset['author'] == 4:
+        if bar_dset['author'] == 5:
+            user_b_label = bar_dset['label']
             user_b_data.append(bar_dset['b_cnt'])
             user_b_data.append(bar_dset['c_cnt'])
             user_b_data.append(bar_dset['r_cnt'])
 
-    print(user_a_data)
-    print(user_b_data)
-
-    context = {'pie_label': pie_label, 'pie_data': pie_data, 'user_a_data': user_a_data, 'user_b_data': user_b_data}
+    context = {'pie_label': pie_label, 'pie_data': pie_data, 'user_a_label': user_a_label, 'user_b_label': user_b_label, 'user_a_data': user_a_data, 'user_b_data': user_b_data}
     return render(request, 'sample/chart_js.html', context)
 
 def api_open(request):
