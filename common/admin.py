6c3447de6818ba1e3d3f359235eae1fe214d4d40
dhfmepd <dhfmepd@gmail.com>
2021-05-08 11:55:40 +0900

    File Admin 추가

--- common/admin.py
+++ common/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, File
 from mptt.admin import DraggableMPTTAdmin
 
+class FileAdmin(admin.ModelAdmin):
+    search_fields = ['file_name']
+
+admin.site.register(File, FileAdmin)
 admin.site.register(Menu, DraggableMPTTAdmin)
