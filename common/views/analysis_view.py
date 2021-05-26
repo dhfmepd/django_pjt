bd4a6b2ec37dc7088aedce1e2d90e5f38bf66d93
gyubeom5062 <kgb6346@naver.com>
2021-05-26 17:53:15 +0900

    add code analysis_view

--- common/views/analysis_view.py
+++ common/views/analysis_view.py
@@ -10,17 +10,17 @@ def analysis_nlp(request):
     자연어 분석
     """
     context = request.POST.get('context', '내용없음')
-        if request.method == 'POST':
-            okt = Okt()
-            result = okt.pos(context, norm=True, stem=True, join=True)
-            data_list = []
-            for word in result:
-                idx = word.find('/')
-                if word[idx + 1:] in ['Noun']:
-                    data_list.append(word[:idx])
+    if request.method == 'POST':
+        okt = Okt()
+        result = okt.pos(context, norm=True, stem=True, join=True)
+        data_list = []
+        for word in result:
+            idx = word.find('/')
+            if word[idx + 1:] in ['Noun']:
+                data_list.append(word[:idx])
 
-            context = {'context': context, 'data_list': data_list}
-            return render(request, 'sample/api_open.html', context)
+        context = {'context': context, 'data_list': data_list}
+        return render(request, 'sample/api_open.html', context)
 
-        context = {'context': context}
+    context = {'context': context}
     return render(request, 'common/analysis_nlp.html', {})
