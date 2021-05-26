1bc0414f34b799142f67c5f10f34b3124174cab9
gyubeom5062 <kgb6346@naver.com>
2021-05-26 17:52:28 +0900

    add code analysis_view

--- common/views/analysis_view.py
+++ common/views/analysis_view.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from konlpy.tag import Okt
+
 @login_required(login_url='common:login')
 
 def analysis_nlp(request):
     """
     자연어 분석
     """
-        context = request.POST.get('context', '내용없음')
+    context = request.POST.get('context', '내용없음')
         if request.method == 'POST':
             okt = Okt()
             result = okt.pos(context, norm=True, stem=True, join=True)
