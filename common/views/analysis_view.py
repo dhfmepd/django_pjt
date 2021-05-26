a713a889f7e0109cd9c31602823b5b2b1e79aaa8
gyubeom5062 <kgb6346@naver.com>
2021-05-26 17:11:16 +0900

    add code analysis_view

--- common/views/analysis_view.py
+++ common/views/analysis_view.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='common:login')
 def index(request):
     return HttpResponse('hello')
-def analysis_nlp(request):
-    """
-    자연어 분석
-    """
-
-    return render(request, 'common/analysis_nlp.html', {})
+# def analysis_nlp(request):
+#     """
+#     자연어 분석
+#     """
+#
+#     return render(request, 'common/analysis_nlp.html', {})
