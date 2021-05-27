3fabae5dad2f809300419a3c7678318a201d616c
gyubeom5062 <kgb6346@naver.com>
2021-05-27 10:30:48 +0900

    add code analysis_view

--- common/views/analysis_view.py
+++ common/views/analysis_view.py
@@ -9,7 +9,7 @@ def analysis_nlp(request):
     """
     자연어 분석
     """
-    param_data = request.POST.get('context', '내용없음')
+    param_data = request.POST.get('param_data', '내용없음')
     if request.method == 'POST':
         okt = Okt()
         result = okt.pos(param_data, norm=True, stem=True, join=True)
@@ -19,8 +19,8 @@ def analysis_nlp(request):
             if word[idx + 1:] in ['Noun']:
                 data_list.append(word[:idx])
 
-        param_data = {'context': param_data, 'data_list': data_list}
+        param_data = {'param_data': param_data, 'data_list': data_list}
         # return render(request, 'sample/api_open.html', context)
 
-    param_data = {'context': param_data}
+    param_data = {'param_data': param_data}
     return render(request, 'common/analysis_nlp.html',param_data)
