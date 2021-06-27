db7a59de63a8654477d2c2a072054d8a41296d87
오동욱 <dhfmepd@gmail.com>
2021-06-28 07:47:18 +0900

    ADMIN 기능 개선

--- common/admin.py
+++ common/admin.py
@@ -1,4 +1,6 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Menu, File, ReceiveHistory, Code
 from mptt.admin import DraggableMPTTAdmin
 
@@ -16,6 +18,23 @@ class CodeAdmin(admin.ModelAdmin):
     search_fields = ['group_code']
     list_display = ['group_code', 'detail_code', 'detail_code_name', 'reference_value', 'use_flag', 'sort_no']
     list_filter = ['group_code']
+    actions = ["export_as_csv"]
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "CSV Export Selected"
+
 
 admin.site.register(File, FileAdmin)
 admin.site.register(Menu, DraggableMPTTAdmin)
